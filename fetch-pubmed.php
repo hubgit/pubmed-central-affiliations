192680db50f33f44eeda672baae3d338c30f7b34
Alf Eaton <alf@hubmed.org>
2012-05-30 14:56:28 +0000

    improve curl_multi

--- fetch-pubmed.php
+++ fetch-pubmed.php
@@ -23,7 +23,8 @@ $curl_multi = curl_multi_init();
 $starts = range(0, $total, 10000);
 
 foreach (array_chunk($starts, 5) as $chunk){
-  // store the file resources so they can be closed when complete
+  // store the curl and file resources so they can be closed when complete
+  $connections = array();
   $files = array();
 
   // build the requests
@@ -35,27 +36,38 @@ foreach (array_chunk($starts, 5) as $chunk){
 
     $curl = curl_init();
 
-    $files[$i] = gzopen($file, 'w');
+    $output = gzopen($file, 'w');
 
     curl_setopt_array($curl, array(
       CURLOPT_URL => 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?' . http_build_query($params),
-      CURLOPT_FILE => $files[$i],
+      CURLOPT_FILE => $output,
       CURLOPT_ENCODING => 'gzip,deflate',
       CURLOPT_VERBOSE => true,
     ));
+
+    $connections[$i] = $curl;
+    $files[$i] = $output;
 
     curl_multi_add_handle($curl_multi, $curl);
   }
 
   // execute the requests
-  $active = false;
+  $active = null;
   do {
-      curl_multi_exec($curl_multi, $active);
-      usleep(100000);
-  } while ($active);
+    $mrc = curl_multi_exec($curl_multi, $active);
+  } while ($mrc == CURLM_CALL_MULTI_PERFORM);
 
-  foreach ($connections as $i => $connection) {
-    curl_close($connection);
+  while ($active && $mrc == CURLM_OK) {
+    if (curl_multi_select($curl_multi) != -1) {
+      do {
+          $mrc = curl_multi_exec($curl_multi, $active);
+      } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+    }
+  }
+
+  foreach ($connections as $i => $curl) {
+    curl_multi_remove_handle($curl_multi, $curl);
+    curl_close($curl);
     gzclose($files[$i]);
   }
 }
